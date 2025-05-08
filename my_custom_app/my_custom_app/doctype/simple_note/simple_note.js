cb62e8fd727c60094f29da59cb3e9d22abede4b7
An'im <anim@venturo.pro>
2025-05-08 14:14:36 +0700

    Initial commit

---
+++ my_custom_app/my_custom_app/doctype/simple_note/simple_note.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Achmad An'im and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Simple Note", {
+// 	refresh(frm) {
+
+// 	},
+// });
